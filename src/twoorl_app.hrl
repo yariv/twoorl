27a2a288501daea1908b87d9093f46d49c18390a
Yariv Sadan <yarivvv@gmail.com>
2008-06-08 16:06:34 -0700

    post-merge tweaks

--- src/twoorl_app.hrl
+++ src/twoorl_app.hrl
@@ -1,7 +1,7 @@
 -define(DB_HOSTNAME, "localhost").
--define(DB_USERNAME, "twoorl").
--define(DB_PASSWORD, "twoorl").
+-define(DB_USERNAME, "root").
+-define(DB_PASSWORD, "password").
 -define(DB_DATABASE, "twoorl").
 -define(DB_POOL_SIZE, 50).
 %-define(APP_PATH, "/home/yariv/apps/twoorl/trunk/twoorl").
--define(APP_PATH, "/Users/yariv/apps/twoorl").
+-define(APP_PATH, "/Users/yariv/Documents/dev/twoorl/trunk/twoorl").
