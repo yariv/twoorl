3ea53921ec2377cd4aef9b5baa0ed344b2240072
yariv <yarivsadan@gmail.com>
2008-12-15 02:06:45 +0000

    added greek and danish

--- src/bundles/twoorl_greek.erl
+++ src/bundles/twoorl_greek.erl
@@ -13,7 +13,7 @@
 %% You should have received a copy of the GNU General Public License
 %% along with Twoorl.  If not, see <http://www.gnu.org/licenses/>.
  
--module(twoorl_eng).
+-module(twoorl_greek).
 -export([bundle/1]).
  
 bundle(Tag) ->
